562512a1322d38e87f36b8666b7da8cc5da95152
Werner Burat <werner.burat@gmail.com>
2018-03-13 16:31:51 -0400

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+    { path:'accueil', component:AccueilComponent},
+    { path:'', redirectTo:'accueil', pathMatch:'full'},
+    { path:'**', redirectTo:'accueil', pathMatch:'full'}, //Si la route est inexistante, rediriger l'utilisateur sur accueil
 ];
 
 @NgModule({
