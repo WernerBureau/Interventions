e9da680c82854203b643e7f73747327b2534791e
Werner Burat <werner.burat@gmail.com>
2018-04-24 15:50:22 -0400

    [TP17] TP17 fini

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -28,7 +28,10 @@ export class ProblemeComponent implements OnInit {
       courrielGroup: this.fb.group({
         zoneCourriel: [{value: '', disabled: true}],
         zoneVerifCourriel: [{value: '', disabled: true}]
-      })
+      }),
+      descriptionProbleme: ['', [Validators.required, Validators.minLength(5)]],
+      noUnite: '',
+      dateProbleme: {value: Date(), disabled: true}
     });
 
     this.probleme.obtenirTypeProbleme()
--- src/app/shared/caracteres-validator.ts
+++ src/app/shared/caracteres-validator.ts
@@ -2,20 +2,22 @@ import { ValidatorFn, AbstractControl } from "@angular/forms";
 
 export class VerifierCaracteresValidator {
     static sansEspaces(): ValidatorFn {
-        return (c: AbstractControl): {[key: string]: boolean } | null => {
-            if (c.value.trim().length != 0 ) {
-                return {'sansEspaces': true};
+        return (c: AbstractControl): { [key: string]: boolean } | null => {
+            if (c.value.trim().length == 0) {
+                return { 'sansEspaces': false };
+            } else {
+                return null;
             }
-            return {'sansEspaces': false};
         };
     }
 
     static longueurMinimum(min: number): ValidatorFn {
-        return (c: AbstractControl): {[key: string]: boolean } | null => {
-            if (c.value.trim().length >= min ) {
-                return {'longueurMinimum': true};
+        return (c: AbstractControl): { [key: string]: boolean } | null => {
+            if (c.value.trim().length >= min) {
+                return null;
+            } else {
+                return { 'longueurMinimum': false };
             }
-            return {'longueurMinimum': false};
         };
     }
 }
